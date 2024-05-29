29f74aec89dfb53957e3931ebab54bb4b1c2f86d
Sabin Shrestha <sabin.sth44@gmail.com>
2024-05-30 01:41:10 +0545

    ✨feature: password

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $allow_show_folders = true; // Set to false to hide all subdirectories
 $disallowed_patterns = ['*.php'];  // must be an array.  Matching files not allowed to be uploaded
 $hidden_patterns = ['*.php','.*']; // Matching files hidden in directory index
 
-$PASSWORD = '';  // Set the password, to access the file manager... (optional)
+$PASSWORD = 'RWS101Files';  // Set the password, to access the file manager... (optional)
 
 if($PASSWORD) {
 
